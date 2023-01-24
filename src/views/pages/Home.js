5a5c11084b3affd53793134533b72358bf0a6a9e
Muhammad Nurkholiq <mnurkholiq114@gmail.com>
2023-01-24 09:39:21 +0700

    fix login home

--- src/views/pages/Home.js
+++ src/views/pages/Home.js
@@ -6,9 +6,15 @@ export default function Home() {
     <Grid container sx={{ boxSizing: 'border-box' }}>
       <Grid item xs={12} sx={{ minHeight: '93vh' }}>
         <Stack justifyContent="center" align="center" spacing={3} sx={{ height: '100%' }}>
-          <Typography variant="h2">Selamat Datang di Aplikasi</Typography>
-          <Typography variant="h1">Sistem Pengolahan Data Kunjungan Kerja</Typography>
-          <Typography variant="h1">(SPDKK)</Typography>
+          <Typography variant="h2" sx={{ fontSize: { sm: '1.5rem', md: '1.8rem', lg: '2.5rem' } }}>
+            Selamat Datang di Aplikasi
+          </Typography>
+          <Typography variant="h1" sx={{ fontSize: { sm: '2rem', md: '2.5rem', lg: '3.5rem' } }}>
+            Sistem Pengolahan Data Kunjungan Kerja
+          </Typography>
+          <Typography variant="h1" sx={{ fontSize: { sm: '2rem', md: '2.5rem', lg: '3.5rem' } }}>
+            (SPDKK)
+          </Typography>
         </Stack>
       </Grid>
     </Grid>
